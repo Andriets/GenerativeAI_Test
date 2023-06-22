5e37a39d62866a199933d5a78df245bfe6a31d29
KolyaNu4 <mykola.andrietc@gmail.com>
2023-06-22 21:37:32 +0300

    UC#2 Create list balance endpoint

---
+++ GenerativeAITest/Controllers/TransactionController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace GenerativeAITest.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TransactionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetListBalance")]
+        public IActionResult GetListBalance()
+        {
+            try
+            {
+                var stripeApiKey = _configuration["StripeApiKeyWrite"];
+
+                StripeConfiguration.ApiKey = stripeApiKey;
+                var balanceService = new BalanceService();
+                var balance = balanceService.Get();
+
+                return Ok(balance);
+            } catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+    }
+}
